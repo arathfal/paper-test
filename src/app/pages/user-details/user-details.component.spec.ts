1352a2b070973cc18bf6f828713c5316d173c259
Aradea Atfal Risdianto <aradea.atfal@shipper.id>
2024-02-12 16:40:56 +0700

    feat: unit test

--- src/app/pages/user-details/user-details.component.spec.ts
+++ src/app/pages/user-details/user-details.component.spec.ts
@@ -1,28 +1,71 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, convertToParamMap } from '@angular/router';
+import { of } from 'rxjs';
+import { UserType } from '../../interfaces/user';
+import { UserService } from '../../services/user-service.service';
 import { UserDetailsComponent } from './user-details.component';
 
 describe('UserDetailsComponent', () => {
   let component: UserDetailsComponent;
   let fixture: ComponentFixture<UserDetailsComponent>;
+  let userService: jasmine.SpyObj<UserService>;
 
   beforeEach(async () => {
+    const userServiceSpy = jasmine.createSpyObj('UserService', ['getUserByID']);
+
     await TestBed.configureTestingModule({
-      imports: [UserDetailsComponent],
+      declarations: [UserDetailsComponent],
       providers: [
         {
           provide: ActivatedRoute,
+          useValue: { snapshot: { paramMap: convertToParamMap({ id: '1' }) } },
         },
+        { provide: UserService, useValue: userServiceSpy },
       ],
     }).compileComponents();
 
+    userService = TestBed.inject(UserService) as jasmine.SpyObj<UserService>;
+  });
+
+  beforeEach(() => {
     fixture = TestBed.createComponent(UserDetailsComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should call getUser method and set user when ngOnInit is called', () => {
+    const userData: UserType = {
+      id: 1,
+      name: 'Leanne Graham',
+      username: 'Bret',
+      email: 'Sincere@april.biz',
+      address: {
+        street: 'Kulas Light',
+        suite: 'Apt. 556',
+        city: 'Gwenborough',
+        zipcode: '92998-3874',
+        geo: {
+          lat: '-37.3159',
+          lng: '81.1496',
+        },
+      },
+      phone: '1-770-736-8031 x56442',
+      website: 'hildegard.org',
+      company: {
+        name: 'Romaguera-Crona',
+        catchPhrase: 'Multi-layered client-server neural-net',
+        bs: 'harness real-time e-markets',
+      },
+    };
+    userService.getUserByID.and.returnValue(of(userData));
+
+    fixture.detectChanges();
+
+    expect(userService.getUserByID).toHaveBeenCalledWith('1');
+    expect(component.user).toEqual({ ...userData, imgUrl: jasmine.any(String) });
+    expect(component.loading).toBe(false);
   });
 });
--- src/app/pages/user-list/user-list.component.spec.ts
+++ src/app/pages/user-list/user-list.component.spec.ts
@@ -1,22 +1,44 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
+import { ActivatedRoute } from '@angular/router'; // Import ActivatedRoute
+import { of } from 'rxjs';
+import { UserService } from '../../services/user-service.service';
 import { UserListComponent } from './user-list.component';
 
 describe('UserListComponent', () => {
+  let component: UserListComponent;
+  let fixture: ComponentFixture<UserListComponent>;
+  let userService: jasmine.SpyObj<UserService>;
+
+  // Create a mock ActivatedRoute object
+  const activatedRouteMock = {
+    snapshot: { paramMap: { get: () => '1' } },
+  };
+
   beforeEach(async () => {
+    const userServiceSpy = jasmine.createSpyObj('UserService', ['getUsers']);
+
     await TestBed.configureTestingModule({
       imports: [UserListComponent],
+      providers: [
+        { provide: UserService, useValue: userServiceSpy },
+        { provide: ActivatedRoute, useValue: activatedRouteMock }, // Provide the mock ActivatedRoute
+      ],
     }).compileComponents();
+
+    userService = TestBed.inject(UserService) as jasmine.SpyObj<UserService>;
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(UserListComponent);
+    component = fixture.componentInstance;
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(UserListComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+    expect(component).toBeTruthy();
   });
 
   it('should render a header with class and text content', () => {
-    const fixture = TestBed.createComponent(UserListComponent);
     const headerElement = fixture.debugElement.query(By.css('.mb-8.text-3xl.text-center'));
     expect(headerElement).toBeTruthy();
 
@@ -25,7 +47,6 @@ describe('UserListComponent', () => {
   });
 
   it('should render the table header with specific columns', () => {
-    const fixture = TestBed.createComponent(UserListComponent);
     const headerRow = fixture.debugElement.query(By.css('thead tr'));
     expect(headerRow).toBeTruthy();
 
@@ -42,5 +63,40 @@ describe('UserListComponent', () => {
         expect(cellClass).toContain('text-left');
       }
     });
+  });
+
+  it('should call getUser method and set users when ngOnInit is called', () => {
+    const userData = [
+      {
+        id: 1,
+        name: 'Leanne Graham',
+        username: 'Bret',
+        email: 'Sincere@april.biz',
+        address: {
+          street: 'Kulas Light',
+          suite: 'Apt. 556',
+          city: 'Gwenborough',
+          zipcode: '92998-3874',
+          geo: {
+            lat: '-37.3159',
+            lng: '81.1496',
+          },
+        },
+        phone: '1-770-736-8031 x56442',
+        website: 'hildegard.org',
+        company: {
+          name: 'Romaguera-Crona',
+          catchPhrase: 'Multi-layered client-server neural-net',
+          bs: 'harness real-time e-markets',
+        },
+      },
+    ];
+    userService.getUsers.and.returnValue(of(userData));
+
+    fixture.detectChanges();
+
+    expect(userService.getUsers).toHaveBeenCalled();
+    expect(component.users).toEqual([...userData]);
+    expect(component.loading).toBe(false);
   });
 });
